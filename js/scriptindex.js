0773dc7bd83718ca8166b668721d40fd9dd9dd37
Lev93 <54747068+Lev93@users.noreply.github.com>
2019-10-22 17:18:13 +0300

    Д/З
    Исправлены замечания Б4,Б8, Б9, Б10, Б11, Д5, Д7, Д8, Д9, Б14, Б15, Б16, Б17, Д17, Д27
    
    Д11, Б12, Д26 не понял, что нужно сделать
    Д13, Д15 будет сделано в конце

--- js/scriptindex.js
+++ js/scriptindex.js
@@ -14,15 +14,29 @@ login.focus();
 close.addEventListener("click", function (evt) {
     evt.preventDefault();
     popup.classList.remove("modal-show");
+	popup.classList.remove("modal-error");
   });
-  form.addEventListener("submit", function (evt) {
+form.addEventListener("submit", function (evt) {
     evt.preventDefault();
   });	
+form.addEventListener("submit", function (evt) {
+    if (!login.value || !email.value) {
+      evt.preventDefault();
+      popup.classList.remove("modal-error");
+      popup.offsetWidth = popup.offsetWidth;
+      popup.classList.add("modal-error");
+    } else {
+      if (isStorageSupport) {
+        localStorage.setItem("login", login.value);
+      }
+    }
+  });
 window.addEventListener("keydown", function (evt) {
     if (evt.keyCode === 27) {
       evt.preventDefault();
       if (popup.classList.contains("modal-show")) {
         popup.classList.remove("modal-show");
+		popup.classList.remove("modal-error");
       }
     }
   });	
