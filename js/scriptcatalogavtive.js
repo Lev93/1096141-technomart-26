c54b412bf4f375d4737038ef081a762d441f3222
Lev93 <54747068+Lev93@users.noreply.github.com>
2019-10-24 13:03:15 +0300

    Д/З

--- js/scriptcatalogavtive.js
+++ js/scriptcatalogavtive.js
@@ -1,9 +1,12 @@
     var openCart = document.querySelectorAll(".buy");
+    var tobookmark = document.querySelectorAll(".product-bookmark");
     var popupCart = document.querySelector(".modal-cart");
     var closeCart = document.querySelector(".modal-cart-close-btn");
     var submitCart = popupCart.querySelector(".modal-cart-btn");
     var countBtn = document.querySelector(".basket");
+    var countbookmark = document.querySelector(".bookmark")
     var counter = countBtn.querySelector(".count");
+    var counterbookmark = countBtn.querySelector(".bookmarkspan");
 
     for (var i = 0; i < openCart.length; i++) {
         openCart[i].addEventListener("click", function (event) {
@@ -22,3 +25,12 @@
         event.preventDefault();
         popupCart.classList.remove("modal-show");
     });
+
+    for (var j = 0; j < tobookmark.length; j++) {
+            tobookmark[j].addEventListener("click", function (e) {
+            e.preventDefault();
+            countbookmark.classList.add("active");
+            var countb = Number(counterbookmark.innerHTML);
+            counterbookmark.innerHTML = countb += 1;
+        });
+    }
