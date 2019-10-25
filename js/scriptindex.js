7d4fd265889b7de9a13057d5f75e5da038f41fa7
Lev93 <54747068+Lev93@users.noreply.github.com>
2019-10-25 15:33:03 +0300

    Финал

--- js/scriptindex.js
+++ js/scriptindex.js
@@ -64,21 +64,25 @@ var login = popup.querySelector("[name=name]");
 var form = popup.querySelector(".contact-form");
 var email = popup.querySelector("[name=email]");
 
+try {
+	storage = localStorage.getItem("login");
+} catch (err) {
+	isStorageSupport = false;
+}
+
 link.addEventListener("click", function (evt) {
 	evt.preventDefault();
 	popup.classList.add("modal-show");
-	setTimeout(function() {
-        login.focus();
-    }, 400);
+	setTimeout(function () {
+		login.focus();
+	}, 400);
 });
 close.addEventListener("click", function (evt) {
 	evt.preventDefault();
 	popup.classList.remove("modal-show");
 	popup.classList.remove("modal-error");
 });
-form.addEventListener("submit", function (evt) {
-	evt.preventDefault();
-});
+
 form.addEventListener("submit", function (evt) {
 	if (!login.value || !email.value) {
 		evt.preventDefault();
@@ -121,7 +125,8 @@ window.addEventListener("keydown", function (evt2) {
 			mapPopup.classList.remove("modal-show");
 		}
 	}
-});var slides = document.querySelectorAll('.slider-item'),
+});
+var slides = document.querySelectorAll('.slider-item'),
 	nextBtn = document.querySelector('.slider-control-button.is-next'),
 	backBtn = document.querySelector('.slider-control-button.is-back'),
 	bulits = document.querySelector('.slider-bulits');
